c1c23fe708e2f035ed2865ef86e3e1364c39d9b2
pruthviraj <marganepruthviraj@gmail.com>
2025-02-19 17:53:26 +0530

    added websocket real time logic

---
+++ apps/ws-backend/src/gameManager.ts
@@ -0,0 +1,201 @@
+import { WebSocket } from "ws";
+
+interface Game {
+  id: string;
+  player1: string;
+  player2: string;
+  rolls: number[];
+  currTurn: string;
+  player1Position: number;
+  player2Position: number;
+  player1Socket: WebSocket | null;
+  player2Socket: WebSocket | null;
+}
+
+const snakes: Record<number, number> = {
+  27: 5,
+  40: 3,
+  43: 18,
+  54: 31,
+  66: 45,
+  76: 58,
+  89: 53,
+  99: 41,
+};
+
+const ladders: Record<number, number> = {
+  4: 25,
+  13: 46,
+  33: 49,
+  50: 69,
+  62: 81,
+  74: 92,
+};
+
+class GameManager {
+  private static gameManagerInstance: GameManager;
+  private games: Map<string, Game> = new Map();
+
+  private constructor() {}
+
+  public static getInstance(): GameManager {
+    if (!this.gameManagerInstance) {
+      this.gameManagerInstance = new GameManager();
+    }
+    return this.gameManagerInstance;
+  }
+
+  public joinGame(userId: string, gameId: string, ws: WebSocket) {
+    let game = this.games.get(gameId);
+
+    if (!game) {
+      game = {
+        id: gameId,
+        player1: userId,
+        player2: "",
+        currTurn: userId,
+        player1Position: 0,
+        player2Position: 0,
+        rolls: [],
+        player1Socket: ws,
+        player2Socket: null,
+      };
+      this.games.set(gameId, game);
+    } else if (!game.player2) {
+      game.player2 = userId;
+      game.player2Socket = ws;
+
+      game.player1Socket?.send(
+        JSON.stringify({ type: "PLAYER_JOINED", userId, game })
+      );
+      game.player2Socket?.send(
+        JSON.stringify({ type: "PLAYER_JOINED", userId, game })
+      );
+    } else {
+      console.log("Game is full!");
+      return;
+    }
+
+    ws.send(
+      JSON.stringify({
+        type: "ROOM_JOINED",
+        userId,
+        game,
+      })
+    );
+  }
+
+  public leaveGame(userId: string, gameId: string) {
+    const game = this.games.get(gameId);
+    if (!game) return;
+
+    if (game.player1 === userId) {
+      game.player1 = "";
+      game.player1Socket = null;
+    } else if (game.player2 === userId) {
+      game.player2 = "";
+      game.player2Socket = null;
+    }
+
+    game.player1Socket?.send(
+      JSON.stringify({ type: "ROOM_LEAVED", userId, gameId })
+    );
+    game.player2Socket?.send(
+      JSON.stringify({ type: "ROOM_LEAVED", userId, gameId })
+    );
+
+    if (!game.player1 && !game.player2) {
+      this.games.delete(gameId);
+    }
+  }
+
+  public rollDice(userId: string, gameId: string) {
+    let game = this.games.get(gameId);
+    if (!game) return;
+
+    if (game.currTurn !== userId) {
+      console.log("Not your turn!");
+      return;
+    }
+
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    game.rolls.push(diceRoll);
+
+    if (userId === game.player1) {
+      game.player1Position += diceRoll;
+
+      game.currTurn = game.player2;
+    } else if (userId === game.player2) {
+      game.player2Position += diceRoll;
+
+      if (snakes[game.player2Position]) {
+        //@ts-ignore
+        game.player2Position = snakes[game.player2Position];
+      }
+
+      if (ladders[game.player2Position]) {
+        //@ts-ignore
+        game.player2Position = ladders[game.player2Position];
+      }
+
+      game.currTurn = game.player1;
+    }
+
+    game.player1Socket?.send(
+      JSON.stringify({ type: "DICE_ROLLED", diceRoll, userId, game })
+    );
+    game.player2Socket?.send(
+      JSON.stringify({ type: "DICE_ROLLED", diceRoll, userId, game })
+    );
+  }
+
+  public snakeAte(userId: string, gameId: string) {
+    let game = this.games.get(gameId);
+    if (!game) return;
+
+    if (game.player1 === userId) {
+      if (snakes[game.player1Position]) {
+        //@ts-ignore
+        game.player1Position = snakes[game.player1Position];
+      }
+    } else if (game.player2 === userId) {
+      if (snakes[game.player2Position]) {
+        //@ts-ignore
+        game.player2Position = snakes[game.player2Position];
+      }
+    }
+
+    game.player1Socket?.send(
+      JSON.stringify({ type: "SNAKE_EATEN_OUT", userId, game })
+    );
+    game.player2Socket?.send(
+      JSON.stringify({ type: "SNAKE_EATEN_OUT", userId, game })
+    );
+  }
+
+  public ladderRise(userId: string, gameId: string) {
+    let game = this.games.get(gameId);
+    if (!game) return;
+
+    if (game.player1 === userId) {
+      if (ladders[game.player1Position]) {
+        //@ts-ignore
+        game.player1Position = ladders[game.player1Position];
+      }
+    } else if (game.player2 === userId) {
+      if (ladders[game.player2Position]) {
+        //@ts-ignore
+        game.player2Position = ladders[game.player2Position];
+      }
+    }
+
+    game.player1Socket?.send(
+      JSON.stringify({ type: "RISED_ON_LADDER", userId, game })
+    );
+    game.player2Socket?.send(
+      JSON.stringify({ type: "RISED_ON_LADDER", userId, game })
+    );
+  }
+}
+
+export default GameManager.getInstance();
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,13 +1,66 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
+import gameManager from "./gameManager";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { config } from "dotenv";
+config();
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+function verifyUser(token: string) {
+  try {
+    const user = verify(
+      token,
+      process.env.NEXTAUTH_SECRET as string
+    ) as JwtPayload;
+    return user.userId;
+  } catch (error) {
+    console.error("Unauthorized User!");
+    return;
+  }
+}
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+wss.on("connection", function connection(ws, req) {
+  const url = new URL(req.url as string);
+  const token = url.searchParams.get("token");
+
+  if (!token) {
+    ws.close();
+    return;
+  }
+
+  ws.on("error", console.error);
+
+  const playerId = verifyUser(token);
+
+  if (!playerId) {
+    ws.send(
+      JSON.stringify({
+        type: "ERROR",
+        message: "Unauthorized User!",
+      })
+    );
+    return;
+  }
+
+  ws.on("message", function message(data: string) {
+    const payload = JSON.parse(data);
+
+    switch (payload.type) {
+      case "JOIN_ROOM":
+        gameManager.joinGame(playerId, payload.gameId, ws);
+        break;
+      case "LEAVE_ROOM":
+        gameManager.leaveGame(playerId, payload.gameId);
+        break;
+      case "ROLL_DICE":
+        gameManager.rollDice(playerId, payload.gameId);
+        break;
+      case "SNAKE_ATE":
+        gameManager.snakeAte(playerId, payload.gameId);
+        break;
+      case "LADDER_FOUND":
+        gameManager.ladderRise(playerId, payload.gameId);
+        break;
+    }
   });
-
-  ws.send('something');
 });
